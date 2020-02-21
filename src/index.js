35d9fd2dee660df135cd23ee8df45b3ff2c95c17
Dmitry Shemiakin <dmitry.shemiakin@zerolab.tech>
2020-02-21 16:34:07 +0300

    feat: completed all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  array = array || [];
+  return (array.length) ? Math.min.apply(null, array) : 0;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  array = array || [];
+  return (array.length) ? Math.max.apply(null, array) : 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  array = array || [];
+  return (array.length) ? array.reduce((a, b) => (a + b)) / array.length : 0;
+};
+
